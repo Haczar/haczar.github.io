c155dd80b791344fc70d70f35e4e4bbf59d1f8eb
Haczar <HaczarCriollo@gmail.com>
2025-04-05 21:34:03 -0400

    updated config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'haczar.github.io', // replace with your actual repo name
+  base: '/', // replace with your actual repo name
   plugins: [react()],
 });
